310e89c989247b41afaaa23816da0d141604e253
Anthony <136890969+merzkiyx@users.noreply.github.com>
2023-12-20 12:18:44 +0800

    Delete uc-flow-node directory

--- uc-flow-node/node/service.py
+++
@@ -1,7 +0,0 @@
-from uc_flow_nodes.service import NodeService
-
-from node.views import info, execute
-class Service(NodeService):
-    class Routes(NodeService.Routes):
-        Info = info.InfoView
-        Execute = execute.ExecuteView
